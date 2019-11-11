d92367e3e46b3dcf0d086897fc2b8db20db95ddc
sjc <songjunchao.cn@gmail.com>
2019-11-11 17:23:46 +0800

    sup init

--- src/views/home/components/sup/Sup.vue
+++ src/views/home/components/sup/Sup.vue
@@ -2,7 +2,9 @@
   <div class="supBox">
     <div class="addSupNum">
       <h1>THANK U!</h1>
-      <AddSup num="{this.props.add.num}" />
+      <div :class="bright?'numSpan'+bright: 'numSpan'">
+        {{num}}
+      </div>
     </div>
     <div ref="imgs">
       <img v-for="item in parrotUrl" :key="item" :src="item" />
@@ -11,18 +13,63 @@
 </template>
 
 <script>
+var PARROTS = 25 // 半径
+var DIAMETER = 30// 放大倍率
+// var ROTATION = 0.1
+// var SPEED = 0.3
+// var SPACING = 4
+var OFFSET = 20
+// var SIZE = 30
+// let parrotsSrcs = []
 export default {
-  name: 'sup',
+  name: 'supMe',
   data () {
-    return {}
+    return {
+      bright: false,
+      num: 1,
+      // gif序列18内取整数,用于堆放文件路径信息
+      parrotsSrcs: [],
+      // 存放位置信息
+      parrots: [],
+      imgLoad: 0,
+      current: OFFSET
+    }
+  },
+  created () {
+
   },
   mounted () {
-    console.log(this.parrotUrl)
+    this.initSrcs()
+    this.initPosition()
+    console.log(this.windowSize)
   },
-  methods: {},
+  methods: {
+    initSrcs () {
+      for (let i = 0; i - PARROTS; i++) {
+        this.parrotsSrcs.push(this.parrotUrl[~~(Math.random() * 18)]) // gif图序列,取18内的整数
+      }
+    },
+    initPosition () {
+      for (var i = 0; i < PARROTS; i++) {
+        this.parrots[i] = { x: 0, y: 0, X: 0, Y: 0 }
+      }
+    }
+  },
   computed: {
     parrotUrl () {
       return this.$imgUrls.parrotUrl
+    },
+    windowSize () {
+      return {
+        w: document.documentElement.clientWidth,
+        h: document.documentElement.clientHeight
+      }
+    },
+    mouse () {
+      return {
+        x: Math.round(this.SIZE * DIAMETER * 0.208333) + OFFSET,
+        y: Math.round(this.SIZE * DIAMETER * 0.208333) + OFFSET
+      }
     }
   }
 }
