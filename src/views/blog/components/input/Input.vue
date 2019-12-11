3265a707272ec8b91dea64581e30afb7bfb9f30d
sjc <songjunchao.cn@gmail.com>
2019-12-11 17:29:14 +0800

    input

--- src/views/blog/components/input/Input.vue
+++ src/views/blog/components/input/Input.vue
@@ -1,5 +1,31 @@
 <template>
-  <div></div>
+  <div class='user-input'>
+        <div class='user-input-label'>
+          <div class='user-emoji'>
+            <span class='user-emoji-icon' @click='showEmojiView'></span>
+            <!-- <CSSTransition
+              in={this.state.showEmoji}
+              key='tests'
+              timeout={200}
+              unmountOnExit
+              classNames="fade"> -->
+              <div ref="emojiView" class='user-emoji-list' @click={selectEmoji(e)}>
+                <!-- {
+                  EMOJI_LIST.map((emoji, i) => <span key={i}>{emoji}</span>)
+                } -->
+              </div>
+            <!-- </CSSTransition> -->
+          </div>
+          <div class='user-input-enter'>
+            <textarea
+              ref="commentText"
+              placeholder="Leave a comment"
+              style={{height: this.state.currentHeight + 'px'}}
+              onKeyUp={(e)=>this.textAutoSize(e)}
+            />
+          </div>
+        </div>
+      </div>
 </template>
 
 <script>
