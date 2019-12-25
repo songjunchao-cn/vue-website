26bc3f35e4b7d200f5072436c60112fbde207e6e
sjc <songjunchao.cn@gmail.com>
2019-12-25 18:20:12 +0800

    workList

---
+++ src/views/work/components/listComponent/listComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="codeBox">
+    <span>{{$attrs.title}}</span>
+    <div class="figureBox">
+      <div class="figure-flex" v-for="item in $attrs.figures" :key="item.src">
+        <figure @click="showItem(item)">
+          <img :src="item.src" alt />
+          <figcaption>{{item.text}}</figcaption>
+        </figure>
+      </div>
+    </div>
+    <hr align="center" width="100%" color="#1e1e1e" size="1" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'listComponent',
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    console.log(this.$attrs, 'attrs')
+  },
+  methods: {}
+}
+</script>
+
+<style lang='scss' scoped>
+@import './listComponent.scss'
+</style>
--- src/views/work/work.vue
+++ src/views/work/work.vue
@@ -1,25 +1,48 @@
 <template>
   <main :class="'page'+scroll">
-        <div style="height:'auto';paddingBottom:'6rem'" class="app-center">
-          <!-- <ListCompon v-if="type!=='code'" title="Desgin" figures={this.state.desginFigu} shows={this.show.bind(this)}  close={this.close.bind(this)}/>
-          <ListCompon v-if="type==='code'" title="Code" figures={this.state.codeFigu} shows={this.show.bind(this)}  close={this.close.bind(this)}/> -->
-          <div @click="toAbout" class="more">
-            <span class="moreIcon">
-              <i></i>
-            </span>
-            <span class="needsclick">VIEW MORE</span>
-          </div>
-          <!-- {this.state.switchin?<Switch type="enter" callback={this.switchOut.bind(this)}/>:''} -->
-        </div>
-      </main>
+    <div style="height:'auto';paddingBottom:'6rem'" class="app-center">
+      <ListComponent title="Work" :figures="workFigure" />
+      <ListComponent title="Code" :figures="codeFigure" />
+      <div @click="toAbout" class="more">
+        <span class="moreIcon">
+          <i></i>
+        </span>
+        <span class="needsclick">VIEW MORE</span>
+      </div>
+      <!-- {this.state.switchin?<Switch type="enter" callback={this.switchOut.bind(this)}/>:''} -->
+    </div>
+  </main>
 </template>
 
 <script>
+import ListComponent from './components/listComponent/listComponent'
+import { workIcon } from '@/components/imgurls'
 export default {
-
+  name: 'work',
+  components: {
+    ListComponent
+  },
+  data () {
+    return {
+      workFigure: [
+        { src: workIcon.logo1, text: '优品养车logo', isShow: false, type: 'white' },
+        { src: workIcon.logo2, text: '优品养车logo', isShow: false, type: 'white' },
+        { src: workIcon.chick, text: '老思鸡H5', isShow: false, type: 'white' },
+        { src: workIcon.moneky, text: '校招H5', isShow: false, type: 'white' }
+      ],
+      codeFigure: [
+        { src: workIcon.game, text: '五子棋', isShow: false, type: 'white' },
+        { src: workIcon.jquo, text: 'jQuo', isShow: false, type: 'white' },
+        { src: workIcon.pictring, text: 'pictring', isShow: false, type: 'white' },
+        { src: workIcon.chick, text: '老思鸡H5', isShow: false, type: 'white' },
+        { src: workIcon.vue, text: 'vue-pictring', isShow: false, type: 'white' },
+        { src: workIcon.blog, text: 'My Blog', isShow: false, type: 'white' }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+@import "@/views/work/work.scss";
 </style>
