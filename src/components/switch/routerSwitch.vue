5e240998d38f3a8e56d48542b9f8f871c501e3f8
sjc <songjunchao.cn@gmail.com>
2020-01-02 18:28:22 +0800

    switchInit

---
+++ src/components/switch/routerSwitch.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="animate-enter">
+      <span v-for="(item,index) in dom" :key="index"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'router-switch',
+  created () {
+    this.initDom()
+  },
+  mounted () {
+  },
+  data () {
+    return {
+      dom: []
+    }
+  },
+  methods: {
+    initDom () {
+      let domList = []
+      for (let index = 0; index < 16; index++) {
+        domList.push({ active: false })
+      }
+      this.dom = domList
+    //   16个矩形
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
--- src/views/root/root.vue
+++ src/views/root/root.vue
@@ -2,17 +2,20 @@
   <div class="root">
     <Header/>
     <router-view></router-view>
+    <router-switch/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '@/components/header/Header'
+import routerSwitch from '@/components/switch/routerSwitch'
 
 export default {
   name: 'root',
   components: {
-    Header
+    Header,
+    routerSwitch
   }
 }
 </script>
