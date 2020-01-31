77b6b388e6a0646d628e8c53d75f8e270d8a773e
sjc <songjunchao.cn@gmail.com>
2020-01-31 13:54:48 +0800

    change ip

--- src/views/blog/components/article/Article.vue
+++ src/views/blog/components/article/Article.vue
@@ -5,7 +5,7 @@
           <a v-if="currentArticle.html_url" target="_blank" :href='currentArticle.html_url'>[原文链接]</a>
           <span class = 'blog-menu-time'>{{$dateFormat(currentArticle.created_at,'yyyy-MM-dd hh:mm')}}</span>
           <Labels :labels='currentArticle.labels'/>
-          <div class='view' v-if="currentArticle.body" v-html='currentArticle.body' />
+          <div class='view' v-if="currentArticle.content" v-html='currentArticle.content' />
           <Comments @changeVisible='changeVisible' :currentArticle= 'currentArticle'/>
         </div>
       </div>
